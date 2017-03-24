489c5c35817fe17308ce7521dd527d8b49b3b526
mochi8k <y.hkh.185@gmail.com>
2017-03-24 12:17:03 +0900

    remove unnecessary files

--- spec/replace-line-break-view-spec.js
+++
@@ -1,9 +0,0 @@
-// 'use babel';
-//
-// import ReplaceLineBreakView from '../lib/replace-line-break-view';
-//
-// describe('ReplaceLineBreakView', () => {
-//   it('has one valid test', () => {
-//     expect('life').toBe('easy');
-//   });
-// });
